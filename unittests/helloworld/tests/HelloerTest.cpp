b1becff484832eee7dbf96c0b494dcdcec4e7793
Dmitry Protopopov <protopopov.da@gmail.com>
2015-10-28 15:48:45 +0300

    Add cppUnit test

---
+++ unittests/helloworld/tests/HelloerTest.cpp
@@ -0,0 +1,33 @@
+/*
+ * File:   HelloerTest.cpp
+ * Author: dmitry
+ *
+ * Created on Oct 28, 2015, 3:39:23 PM
+ */
+
+#include "HelloerTest.h"
+#include "Helloer.h"
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(HelloerTest);
+
+HelloerTest::HelloerTest() {
+}
+
+HelloerTest::~HelloerTest() {
+}
+
+void HelloerTest::setUp() {
+}
+
+void HelloerTest::tearDown() {
+}
+
+void HelloerTest::testMessage() {
+    Helloer helloer("Netbeans");
+    string result = helloer.message();
+    if (true /*check result*/) {
+        CPPUNIT_ASSERT(result == "Hello Netbeans");
+    }
+}
+
---
+++ unittests/helloworld/tests/HelloerTest.h
@@ -0,0 +1,32 @@
+/*
+ * File:   HelloerTest.h
+ * Author: dmitry
+ *
+ * Created on Oct 28, 2015, 3:39:23 PM
+ */
+
+#ifndef HELLOERTEST_H
+#define	HELLOERTEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class HelloerTest : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(HelloerTest);
+
+    CPPUNIT_TEST(testMessage);
+
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    HelloerTest();
+    virtual ~HelloerTest();
+    void setUp();
+    void tearDown();
+
+private:
+    void testMessage();
+
+};
+
+#endif	/* HELLOERTEST_H */
+
