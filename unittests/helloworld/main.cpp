91f1afe0aaf8b48bf4d031ac37e0dd5cfd323016
Dmitry Protopopov <protopopov.da@gmail.com>
2015-10-28 14:53:56 +0300

    Add build scripts

---
+++ unittests/helloworld/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: dmitry
+ *
+ * Created on October 28, 2015, 2:45 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
